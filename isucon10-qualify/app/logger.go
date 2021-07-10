4bfbdcbbd97987eeb7a07b0bdb4947f999e3ce6b
Mayo <mayocream39@yahoo.co.jp>
2021-07-10 13:08:35 +0800

    chore: logger time format

--- isucon10-qualify/app/logger.go
+++ isucon10-qualify/app/logger.go
@@ -11,6 +11,7 @@ func initLogger() {
 	conf := zap.NewProductionConfig()
 	conf.OutputPaths = []string{"/var/log/isucon/app.log"}
 	conf.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	conf.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	l, _ := conf.Build()
 	logger = l.Sugar()
 }
