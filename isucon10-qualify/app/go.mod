8cc06ded6c20dc89ba4918ad2d16225b776381b3
Mayo <mayocream39@yahoo.co.jp>
2021-07-22 16:48:00 +0800

    feat(app):redis

--- isucon10-qualify/app/go.mod
+++ isucon10-qualify/app/go.mod
@@ -11,9 +11,17 @@ require (
 	github.com/json-iterator/go v1.1.11
 	github.com/kellydunn/golang-geo v0.7.0
 	github.com/kylelemons/go-gypsy v1.0.0 // indirect
+	github.com/lib/pq v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cast v1.3.1
-	github.com/tal-tech/go-zero v1.1.8
 	github.com/ziutek/mymysql v1.5.4 // indirect
 	go.uber.org/zap v1.18.1
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/tools v0.1.3 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- isucon10-qualify/app/redis.go
+++ isucon10-qualify/app/redis.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -12,75 +13,122 @@ import (
 
 // cache key
 const (
-	CacheKeyChairID = "chair:id:"
-	CacheKeyEstateID = "estate:id:"
+    CacheKeyChairID = "chair:id:"
+    CacheKeyEstateID = "estate:id:"
 )
 
 var redisClient *redis.Client
 
+func cacheKey(arr ...string) string {
+    return strings.Join(arr, ":")
+}
+
 func initRedis() {
-	addr := os.Getenv("REDIS_ADDR")
-	if addr == "" {
-		logger.Error("Redis addr env empty")
-		return
-	}
+    addr := os.Getenv("REDIS_ADDR")
+    if addr == "" {
+        logger.Error("Redis addr env empty")
+        return
+    }
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr: addr,
-	})
+    rdb := redis.NewClient(&redis.Options{
+        Addr: addr,
+    })
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second) 
-	defer cancel()
-	if err := rdb.Ping(ctx); err != nil {
-		logger.Errorf("Redis ping err: %s", err)
-	}
+    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second) 
+    defer cancel()
+    if err := rdb.Ping(ctx); err != nil {
+        logger.Errorf("Redis ping err: %s", err)
+    }
 
-	redisClient = rdb
+    redisClient = rdb
 }
 
 func tablesCache() {
-	{
-		query := "SELECT * FROM chair"
-		data := make([]Chair, 0, 3200)
-		if err := db.Select(&data, query); err != nil {
-			logger.Errorf("chair table query err: %s", err)
-		}
-	
-		pipe := redisClient.Pipeline()
-		for _, row := range data {
-			cacheRow(CacheKeyChairID, row.ID, row, pipe)
-		}
-		if _, err := pipe.Exec(context.Background()); err != nil {
-			logger.Errorf("redis cache chair err: %s", err)
-		}
-	}
+    {
+        query := "SELECT * FROM chair"
+        data := make([]*Chair, 0, 3200)
+        if err := db.Select(&data, query); err != nil {
+            logger.Errorf("chair table query err: %s", err)
+        }
+    
+        pipe := redisClient.Pipeline()
+        for _, row := range data {
+            cacheRow(CacheKeyChairID, row.ID, row, pipe)
+        }
+        if _, err := pipe.Exec(context.Background()); err != nil {
+            logger.Errorf("redis cache chair err: %s", err)
+        }
 
-	{
-		query := "SELECT * FROM estate"
-		data := make([]Estate, 0, 3200)
-		if err := db.Select(&data, query); err != nil {
-			logger.Errorf("estate table query err: %s", err)
-		}
-	
-		pipe := redisClient.Pipeline()
-		for _, row := range data {
-			cacheRow(CacheKeyChairID, row.ID, row, pipe)
-		}
-		if _, err := pipe.Exec(context.Background()); err != nil {
-			logger.Errorf("redis cache estate err: %s", err)
-		}
-	}
+        if err := cacheChair(data...); err != nil {
+            logger.Errorf("cache chair err: %s", err)
+        }
+    }
+
+    {
+        query := "SELECT * FROM estate"
+        data := make([]Estate, 0, 3200)
+        if err := db.Select(&data, query); err != nil {
+            logger.Errorf("estate table query err: %s", err)
+        }
+    
+        pipe := redisClient.Pipeline()
+        for _, row := range data {
+            cacheRow(CacheKeyChairID, row.ID, row, pipe)
+        }
+        if _, err := pipe.Exec(context.Background()); err != nil {
+            logger.Errorf("redis cache estate err: %s", err)
+        }
+    }
 }
 
 func cacheRow(prefix string, id interface{}, row interface{}, r redis.Pipeliner) {
-	val, _ := jsoniter.MarshalToString(row)
-	var err error
-	if r != nil {
-		err = r.Set(context.Background(), prefix + cast.ToString(id), val, 3600 * time.Second).Err()
-	} else {
-		err = redisClient.Set(context.Background(), prefix + cast.ToString(id), val, 3600 * time.Second).Err()
-	}
-	if err != nil {
-		logger.Errorf("redis cache row err: %s", err)
-	}
+    val, _ := jsoniter.MarshalToString(row)
+    var err error
+    if r != nil {
+        err = r.Set(context.Background(), prefix + cast.ToString(id), val, 0).Err()
+    } else {
+        err = redisClient.Set(context.Background(), prefix + cast.ToString(id), val, 
+        0).Err()
+    }
+    if err != nil {
+        logger.Errorf("redis cache row err: %s", err)
+    }
+}
+
+func fetchCacheRow(prefix string, id interface{}, data interface{}) error {
+    val, err := redisClient.Get(context.Background(), prefix + cast.ToString(id)).Result()
+    if err != nil {
+        return err
+    }
+    if err := jsoniter.UnmarshalFromString(val, data); err != nil {
+        return err
+    }
+    return nil
+}
+
+func cacheChair(arr ...*Chair) error {
+    pipe := redisClient.Pipeline()
+    for _, row := range arr {
+        pipe.SAdd(context.Background(), cacheKey("chair", "color", row.Color), row.ID)
+        pipe.SAdd(context.Background(), cacheKey("chair", "kind", row.Kind), row.ID)
+        pipe.ZAdd(context.Background(), cacheKey("chair", "price"), &redis.Z{
+            Score: float64(row.Price),
+            Member: row.ID,
+        })
+        pipe.ZAdd(context.Background(), cacheKey("chair", "height"), &redis.Z{
+            Score: float64(row.Height),
+            Member: row.ID,
+        })
+        pipe.ZAdd(context.Background(), cacheKey("chair", "width"), &redis.Z{
+            Score: float64(row.Width),
+            Member: row.ID,
+        })
+        pipe.ZAdd(context.Background(), cacheKey("chair", "depth"), &redis.Z{
+            Score: float64(row.Depth),
+            Member: row.ID,
+        })
+        pipe.Set(context.Background(), cacheKey("chair", "popularity", cast.ToString(row.ID)), row.Popularity, 0)
+        pipe.Set(context.Background(), cacheKey("chair", "stock", cast.ToString(row.ID)), row.Stock, 0)
+    }
+    return nil
 }
